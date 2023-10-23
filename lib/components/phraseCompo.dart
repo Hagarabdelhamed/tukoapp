d08c01e03ebfcc260e9ef70db63fe1898b1cca87
Hagarabdelhamed <83194017+Hagarabdelhamed@users.noreply.github.com>
2023-10-23 17:54:28 +0300

    Phrase Compontent

---
+++ lib/components/phraseCompo.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:tukoapp/models/PageModel.dart';
+
+import 'itemCompo.dart';
+
+class PhrasesCompo extends StatelessWidget {
+  const PhrasesCompo({super.key, required this.phrase});
+  final PageModel phrase;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff47A5CB),
+      height: 90,
+      child: Row(
+        children: [
+          Expanded(child: ItemComp(item: phrase)),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/phrases.dart
+++ lib/screens/phrases.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:tukoapp/models/PageModel.dart';
 
 class PhrasesPage extends StatelessWidget {
-  const PhrasesPage({super.key});
+   PhrasesPage({super.key});
+   List<PageModel> phrase =[
+    PageModel(sound: sound, japText: japText, engText: engText)
+   ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xffFFF4D9),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff49332A),
+        title: Text('Phrase'),
+      ),
+    );
   }
 }
